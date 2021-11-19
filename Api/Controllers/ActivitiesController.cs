de4112c17b8b81bb9f48f3680f80998340bdafdc
Óscar Tutor Rubio <otutor@zimaltec.es>
2021-11-19 01:40:52 +0100

    added react client app

--- Api/Controllers/ActivitiesController.cs
+++ Api/Controllers/ActivitiesController.cs
@@ -8,6 +8,7 @@ namespace Api.Controllers
     public class ActivitiesController : BaseApiController
     {
         private readonly DataContext _context;
+        
         public ActivitiesController(DataContext context)
         {
             _context = context;
